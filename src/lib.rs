c1230c34f1e6b9e624ca78dd5fa2aa68f8449fbb
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-12-30 17:09:16 -0500

    feat: basic parsing grammar

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+#[macro_use]
+// std
+
+// crates
+use pest::Parser;
+use pest_derive::*;
+
+// local
+
+#[derive(Parser)]
+#[grammar = "lib.pest"]
+pub struct CharonParser;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
